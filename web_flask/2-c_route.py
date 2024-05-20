de4345d3083d04711b0226b2e81ee58dab2ce449
root <root@Y4x>
2024-05-20 02:41:32 +0100

    fix 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,29 +1,36 @@
 #!/usr/bin/python3
 """
-start Flask application
+hello Flask!
 """
-
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
-    return 'Hello HBNB!'
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """
+    hello HBNB
+    """
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
-    return 'HBNB'
+    """
+    HBNB
+    """
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
-    return 'C ' + text.replace('_', ' ')
+@app.route("/c/<text>", strict_slashes=False)
+def widlcard(text):
+    """
+    Widlcard usage
+    """
+    text = text.replace("_", " ")
+    return f"C {text}"
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
-    
+    app.run(host="0.0.0.0", port=5000, debug=True)
